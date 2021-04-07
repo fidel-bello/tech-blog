81b2152b0e512b4d6b9b40861cc083c83c976e1a
fidel bello <59423@bears.smcc.edu>
2021-04-07 11:20:43 -0400

    created connection to database and server

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if(process.env.JAWSDB_URL) {
+sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize (process.env.DB, process.env.USER, process.env.PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+module.exports = sequelize;
