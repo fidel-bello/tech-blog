4d0a0536bb8a2adf02576903f9fbed9775d6f6de
fidel bello <59423@bears.smcc.edu>
2021-04-08 17:57:12 -0400

    added a post table

--- models/index.js
+++ models/index.js
@@ -1,9 +1,30 @@
 const User = require('./user');
 const Comment = require('./comment');
+const Post = require('./post')
+
+//Table Relationships
+
+Comment.belongsTo(Post, {
+    foreignKey: 'user_id'
+});
+
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
 User.hasMany(Comment, {
     foreignKey: 'user_id'
-})
-module.exports = {User,Comment}
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+module.exports = {User,Comment, Post}
