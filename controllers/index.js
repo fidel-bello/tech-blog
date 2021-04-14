f81a0cdb226e2f3c9510a57d5de0ba776ddce999
fidel bello <59423@bears.smcc.edu>
2021-04-13 20:02:17 -0400

    added routers for the home index

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeroutes');
-const dashRoutes = require()
+const dashRoutes = require('./dashRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('./dashboard', dashRoutes);
+
 router.use((req,res) => {
     res.status(404).end();
 })
