8b6e7d36a090ee26e84e37853ac34c39197a7cb6
fidel bello <59423@bears.smcc.edu>
2021-04-13 20:00:16 -0400

    added routers for api index file

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require ('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./post');
+const commentRoutes = require('./comments');
+
+router.use('/users', userRoutes);
+router.use('posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports= router;
